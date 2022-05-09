ef1085487e21aee3c011dea3c3e8324390c6f8ff
Sven Liebert <mail@sven-liebert.de>
2022-05-09 08:55:16 +0200

    Only select elements with "Pferd" or "pferd" included

--- src/content.js
+++ src/content.js
@@ -1,18 +1,20 @@
-const tags=["h1","h2","h3","a","p","title","div","button"];
-const namessing=["Hufeneisenhuso","Gemeiner Gaul","Hufratte"]
-const namesplu=["Hufeneisenhusos","Gemeine Gäule","Hufratten"]
-for(const tag of tags){
-    const elements=document.getElementsByTagName(tag);
-    for(const element of elements){
-        let text=element.innerHTML;
-        
-        let result = text.replace(/Pferde/g,namesplu[Math.floor(Math.random()*namesplu.length)]);
-        result = result.replace(/Pferd/g,namessing[Math.floor(Math.random()*namessing.length)]);
+const tags = ["h1", "h2", "h3", "a", "p", "title", "div", "button"];
+const namessing = ["Hufeneisenhuso", "Gemeiner Gaul", "Hufratte"]
+const namesplu = ["Hufeneisenhusos", "Gemeine Gäule", "Hufratten"]
+let element = null;
+for (const tag of tags) {
+    const elements = document.evaluate('//*[contains(text(),\'Pferd\') or contains(text(),\'pferd\')]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (let i = 0; i < elements.snapshotLength; i++) {
+        element = elements.snapshotItem(i);
+        let text = element.innerText;
 
-        result = result.replace(/pferde/g,namesplu[Math.floor(Math.random()*namesplu.length)].toLowerCase());
-        result = result.replace(/pferd/g,namessing[Math.floor(Math.random()*namessing.length)].toLowerCase());
+        let result = text.replace(/Pferde/g, namesplu[Math.floor(Math.random() * namesplu.length)]);
+        result = result.replace(/Pferd/g, namessing[Math.floor(Math.random() * namessing.length)]);
 
-        element.innerHTML=result;
+        result = result.replace(/pferde/g, namesplu[Math.floor(Math.random() * namesplu.length)].toLowerCase());
+        result = result.replace(/pferd/g, namessing[Math.floor(Math.random() * namessing.length)].toLowerCase());
+
+        element.innerHTML = result;
     }
 
 }
